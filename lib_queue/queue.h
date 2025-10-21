9829ecad8f6b9dd685d02b5f902920ca4ee1268d
midi <fragemorenno@gmail.com>
2025-10-21 11:56:34 +0300

    finaly queue with tests

--- lib_queue/queue.h
+++ lib_queue/queue.h
@@ -1,56 +1,65 @@
-	#pragma once
+#pragma once
 
 template <class T>
 class Queue {
-	T* _data;
-	int _head;
-	int _count;
-	int _size;
+    T* _data;
+    int _head;
+    int _count;
+    int _size;
 public:
-	Queue();
-	Queue(int size);
-	Queue(const Queue& oth);
-	void push(const T& elem);
-	void pop();
-	inline T& head();
-	bool is_empty() { return _count == 0; }
-	bool is_full() { return _count == _size; }
-	void clear() { _head = 0, _count = 0; }
-	
-	~Queue() { delete[] _data; }
+    Queue();
+    Queue(int size);
+    Queue(const Queue& oth);
+    void push(const T& elem);
+    void pop();
+    inline T& head();
+    bool is_empty() { return _count == 0; }
+    bool is_full() { return _count == _size; }
+    void clear() { _head = 0, _count = 0; }
+    int size() { return _size; }
+    int count() { return _count; }
+    T* data() { 
+        return _data;
+    }
 
-
+    ~Queue() { delete[] _data; }
 };
+
 template <class T>
 void Queue<T>::push(const T& elem) {
-	if (is_full()) {
-		throw std::logic_error("out of range");
-	}
-	_count++;
-	_data[(_head+_cout-1)% _size] = elem;
+    if (is_full()) {
+        throw std::logic_error("out of range");
+    }
+    _count++;
+    _data[(_head + _count - 1) % _size] = elem; 
 }
+
 template <class T>
 void Queue<T>::pop() {
-	if (is_empty()) {
-		throw std::logic_error("queue is empty");
-	}
-	_count--;
-	_head = (++_head) % _size;
+    if (is_empty()) {
+        throw std::logic_error("queue is empty");
+    }
+    _count--;
+    _head = (++_head) % _size;
 }
+
 template <class T>
-Queue<T>::Queue() : _data(nullptr), _size(0), _count(0), _head(0);
+Queue<T>::Queue() : _data(nullptr), _size(0), _count(0), _head(0) {}
+
 template <class T>
 Queue<T>::Queue(int size) : _count(0), _head(0), _size(size) {
-	_data = new T[_size];
+    _data = new T[_size];
 }
+
 template <class T>
-Queue<T>::Queue(const Queue& oth): _head(oth._head), _count(oth._count), _size(oth._size) {
-	_data = new T[_size];
-	for (int i = 0; i < _count; i++) {
-		_data[i] = oth._data[i];
-	}
+Queue<T>::Queue(const Queue& oth) : _head(oth._head), _count(oth._count), _size(oth._size) {
+    _data = new T[_size];
+    for (int i = 0; i < _count; i++) {
+        _data[i] = oth._data[i];
+    }
 }
+
 template <class T>
 inline T& Queue<T>::head() {
-	return _data[_head];
+    return _data[_head];
 }
--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -3,7 +3,120 @@
 #include <gtest/gtest.h>
 #include "../lib_easy_example/easy_example.h"
 #include "queue.h"
-TEST(TestQueueLib, can_create_with_init_constructor) {
-	ASSERT_NO_THROW(Queue<int> Q(20));
+TEST(TestQueueLib, can_create_default_queue) {
+    Queue<int> Q;
+    EXPECT_TRUE(Q.is_empty());
+    EXPECT_EQ(Q.size(), 0);
+}
 
+TEST(TestQueueLib, can_create_queue_with_size) {
+    Queue<int> Q(10);
+    EXPECT_TRUE(Q.is_empty());
+    EXPECT_FALSE(Q.is_full());
+    EXPECT_EQ(Q.size(), 10);
+}
+
+TEST(TestQueueLib, can_push_elements) {
+    Queue<int> Q(5);
+
+    Q.push(1);
+    EXPECT_FALSE(Q.is_empty());
+    EXPECT_EQ(Q.count(), 1);
+
+    Q.push(2);
+    Q.push(3);
+    EXPECT_EQ(Q.count(), 3);
+}
+
+TEST(TestQueueLib, can_pop_elements) {
+    Queue<int> Q(5);
+    Q.push(10);
+    Q.push(20);
+    Q.push(30);
+
+    EXPECT_EQ(Q.head(), 10);
+    Q.pop();
+    EXPECT_EQ(Q.head(), 20);
+    EXPECT_EQ(Q.count(), 2);
+
+    Q.pop();
+    EXPECT_EQ(Q.head(), 30);
+    EXPECT_EQ(Q.count(), 1);
+}
+
+TEST(TestQueueLib, can_handle_circular_behavior) {
+    Queue<int> Q(3);
+    Q.push(1);
+    Q.push(2);
+    Q.push(3);
+
+    Q.pop();
+    Q.push(4); 
+
+    EXPECT_EQ(Q.head(), 2);
+    Q.pop();
+    EXPECT_EQ(Q.head(), 3);
+    Q.pop();
+    EXPECT_EQ(Q.head(), 4);
+}
+
+TEST(TestQueueLib, push_throws_when_full) {
+    Queue<int> Q(2);
+    Q.push(1);
+    Q.push(2);
+
+    EXPECT_THROW(Q.push(3), std::logic_error);
+}
+
+TEST(TestQueueLib, pop_throws_when_empty) {
+    Queue<int> Q(5);
+    EXPECT_THROW(Q.pop(), std::logic_error);
+
+    Q.push(1);
+    Q.pop();
+    EXPECT_THROW(Q.pop(), std::logic_error);
+}
+
+TEST(TestQueueLib, can_clear_queue) {
+    Queue<int> Q(5);
+    Q.push(1);
+    Q.push(2);
+    Q.push(3);
+
+    EXPECT_FALSE(Q.is_empty());
+    Q.clear();
+    EXPECT_TRUE(Q.is_empty());
+    EXPECT_EQ(Q.count(), 0);
+}
+TEST(TestQueueLib, is_full_works_correctly) {
+    Queue<int> Q(3);
+    EXPECT_FALSE(Q.is_full());
+
+    Q.push(1);
+    EXPECT_FALSE(Q.is_full());
+
+    Q.push(2);
+    EXPECT_FALSE(Q.is_full());
+
+    Q.push(3);
+    EXPECT_TRUE(Q.is_full());
+
+    Q.pop();
+    EXPECT_FALSE(Q.is_full());
+}
+TEST(TestQueueLib, stress_test) {
+    Queue<int> Q(1000);
+
+    // Заполняем очередь
+    for (int i = 0; i < 1000; i++) {
+        Q.push(i);
+    }
+    EXPECT_TRUE(Q.is_full());
+
+    // Опустошаем очередь
+    for (int i = 0; i < 1000; i++) {
+        EXPECT_EQ(Q.head(), i);
+        Q.pop();
+    }
+    EXPECT_TRUE(Q.is_empty());
 }
