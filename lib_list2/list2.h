a184fac7d344556e8d3f1e8a525a20b652c49e9d
midi <fragemorenno@gmail.com>
2025-11-26 13:32:15 +0300

    copy

--- lib_list2/list2.h
+++ lib_list2/list2.h
@@ -80,6 +80,11 @@ public:
 
     ListDoubly() : _head(nullptr), _tail(nullptr), _count(0) {};
 
+    ListDoubly(const ListDoubly& other) : _head(nullptr), _tail(nullptr), _count(0) {
+        for (Node<T>* cur = other._head; cur != nullptr; cur = cur->next) {
+            push_back(cur->value);
+        }
+    }
 
     ~ListDoubly() {
         clear();
