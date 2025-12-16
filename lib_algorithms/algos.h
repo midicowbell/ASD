9d6a8ce3e18a033a5ab888892f78c9cb633f5524
midi <fragemorenno@gmail.com>
2025-12-16 20:55:49 +0300

    push

--- lib_algorithms/algos.h
+++ lib_algorithms/algos.h
@@ -1,3 +1,30 @@
-#include <vector>
 #include "dsu.h"
-int count_islands(std::vector<std::vector<int>>& field);
+#include <vector>
+
+int count_islands(std::vector<std::vector<int>>& field) {
+	if (field.empty()) { return 0; }
+	int rows = field.size();
+	int cols = field[0].size();
+	DSU dsu(rows * cols);
+	int islandCount = 0;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (field[i][j] == 1) {
+				islandCount++;
+				if (j + 1 < cols && field[i][j + 1] == 1) {
+					if (dsu.find(i * cols + j) != dsu.find(i * cols + (j + 1))) {
+						dsu.unite(i * cols + j, i * cols + (j + 1));
+						islandCount--;
+					}
+				}
+				if (i + 1 < rows && field[i + 1][j] == 1) {
+					if (dsu.find(i * cols + j) != dsu.find((i + 1) * cols + j)) {
+						dsu.unite(i * cols + j, (i + 1) * cols + j);
+						islandCount--;
+					}
+				}
+			}
+		}
+	}
+	return islandCount;
+}
